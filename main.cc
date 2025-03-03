addd37ab1b3117d8b16bd2954844a6b9ce4490fc
imariom <marioallfred@gmail.com>
2025-03-04 01:48:07 +0200

    project setup

--- main.cc
+++ main.cc
@@ -1,10 +1,66 @@
-#include <iostream>
 #include <boost/asio.hpp>
+#include <boost/json.hpp>
+#include <boost/json/src.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <cstdint>
+#include <optional>
+
+namespace brainiac {
+
+namespace net = boost::asio;
+
+enum class config_hint : unsigned short
+{
+    unknown = 0,
+
+    allowed_ips,
+    allowed_ports,
+};
+
+class ServerConfig
+{
+public:
+    ServerConfig()
+    {
+        // load the default "brainiac.yaml" configuration file
+        std::ifstream configFile("brainiac.json");
+        if (configFile)
+        {
+        }
+    }
+
+    template<typename ConfigValue>
+    void set(config_hint hint, const ConfigValue& value)
+    {
+        config_[hint] = value;
+    }
+
+    template<typename ConfigValue>
+    void set(config_hint hint, ConfigValue&& value)
+    {
+        config_[hint] = std::forward<ConfigValue>(value);
+    }
+
+    template<typename ConfigValue>
+    std::optional<ConfigValue> get(config_hint hint) const
+    {
+    }
+
+
+private:
+    using config_value = std::variant<
+        std::size_t
+    >;
+
+    std::unordered_map<config_hint, config_value> config_;
+};
 
 class ProxySession
 {
 public:
-    ProxySession(std::shared_ptr<boost::asio::ip::tcp::socket> socketPtr)
+    ProxySession()
     {
     }
 
@@ -16,66 +72,56 @@ public:
 class ProxyServer
 {
 public:
-    ProxyServer(boost::asio::io_context& ioContext, std::uint16_t portNumber)
-        : _acceptor{ioContext}, _ioContext{ioContext}
+    ProxyServer(ServerConfig config)
+        : config_(config)
     {
-        initialize(portNumber);
-        startAccept();
+        // if user provided a list of IPs to use
+        // if the user didn't provide the IP (use all available IPs in the host)
+        // if user provided port number use it otherwise 5437
+
+        // pass the endpoint
+        net::ip::tcp::endpoint endpoint();
+
+        acceptor_ = std::make_shared<net::ip::tcp::acceptor>(endpoint);
+    }
+
+    void run()
+    {
+        start_accept();
     }
 
 private:
-    void initialize(std::uint16_t portNumber)
+    void start_accept()
     {
-        boost::asio::ip::tcp::endpoint endpoint {
-            boost::asio::ip::tcp::v4(),
-            portNumber
-        };
-
-        _acceptor.open(endpoint.protocol());
-        _acceptor.bind(endpoint);
-        _acceptor.listen();
+        acceptor_->async_accept()
     }
 
-    void startAccept()
-    {
-        auto socketPtr = std::make_shared<boost::asio::ip::tcp::socket>(_ioContext);
-
-        _acceptor.async_accept(*socketPtr.get(),
-            [this, socketPtr](const boost::system::error_code& errCode)
-            {
-                if (!errCode)
-                    std::make_shared<ProxySession>(socketPtr)->start();
-                else
-                    std::cerr << "Failed to accept: " << errCode.value()
-                        << ". Message: " << errCode.message();
-                startAccept();
-            });
-    }
-
-private:
-    boost::asio::ip::tcp::acceptor _acceptor;
-    boost::asio::io_context& _ioContext;
+    const ServerConfig& config_;
+    std::shared_ptr<net::ip::tcp::acceptor> acceptor_;
+    std::shared_ptr<net::ip::tcp::socket> socket_;
 };
+
+
+} // namespace brainiac
 
 int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: proxy_server <port>\n";
-        return -1;
+        std::cerr << "Usage: brainiac <port>\n"
+            << "    Example: brainiac 5437\n";
+        return EXIT_FAILURE;
     }
     std::uint16_t portNumber = std::atoi(argv[1]);
 
-    try
-    {
-        boost::asio::io_context ioContext{};
-        ProxyServer server{ioContext, portNumber};
+    // load the default "brainiac.yaml" configuration file
+    brainiac::ServerConfig config;
+    config.allowed_ips();
+    config.allowed_port();
 
-        ioContext.run();
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << '\n';
-        return -1;
-    }
+    brainiac::ProxyServer server(config);
+    server.run();
+
+    return EXIT_SUCCESS;
 }
+
